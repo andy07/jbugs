1fbc03288c166b5388f709d2a14adc3a28b0634c
Alexandra Dragos <alexandradragos2012@gmail.com>
2019-05-22 18:35:46 +0300

    create bug packages and classes: + InputDto + OutputDto + DAO + Control + Facade

---
+++ jbugs-business/src/main/java/msg/bug/control/BugControl.java
@@ -0,0 +1,10 @@
+package msg.bug.control;
+
+/**
+ * Document me.
+ *
+ * @author msg systems AG; User Name.
+ * @since 19.1.2
+ */
+public class BugControl {
+}
---
+++ jbugs-business/src/main/java/msg/bug/entity/dto/BugOutputDTO.java
@@ -0,0 +1,10 @@
+package msg.bug.entity.dto;
+
+/**
+ * Document me.
+ *
+ * @author msg systems AG; User Name.
+ * @since 19.1.2
+ */
+public class BugOutputDTO {
+}
