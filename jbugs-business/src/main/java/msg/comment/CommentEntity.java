4dcde80d149846844f7a650c3b7c8061b8dffbb1
Alexandra Dragos <alexandradragos2012@gmail.com>
2019-05-22 16:38:51 +0300

    first commit from alexandra

--- jbugs-business/src/main/java/msg/comment/CommentEntity.java
+++ jbugs-business/src/main/java/msg/comment/CommentEntity.java
@@ -18,6 +18,7 @@ public class CommentEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+
     @Column(name = "date", nullable = false)
     private Date date;
 
