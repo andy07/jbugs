ee0f6500dfecc407d8bfc8af5e99b2b3263ece5e
Florina Diana Stefan <stefandiana0397@gmail.com>
2019-05-22 20:35:53 +0300

    BugStatusTest1

---
+++ jbugs-business/src/test/java/msg/bug/BugStatusTest.java
@@ -0,0 +1,27 @@
+package msg.bug;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Document me.
+ *
+ * @author msg systems AG; User Name.
+ * @since 19.1.2
+ */
+public class BugStatusTest {
+
+    @Test
+    public void getStatus() {
+    }
+
+    @Test
+    public void getNextStatusAllowedList() {
+    }
+
+    @Test
+    public void isAllowedStatusFromTo() {
+    }
+}
