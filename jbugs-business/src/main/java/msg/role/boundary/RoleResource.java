b519b354b603173363a1b18707f615247a9a851d
Florina Diana Stefan <stefandiana0397@gmail.com>
2019-06-03 19:34:38 +0300

    permisiuni

--- jbugs-business/src/main/java/msg/role/boundary/RoleResource.java
+++ jbugs-business/src/main/java/msg/role/boundary/RoleResource.java
@@ -28,6 +28,7 @@ public class RoleResource {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @StarkPermissions(permission = StarkPermissions.Permission.PERMISSION_MANAGEMENT)
     public Response updateRole(RoleDTO roleDTO){
         roleFacade.updateRole(roleDTO);
         return Response.ok().build();
@@ -45,6 +46,7 @@ public class RoleResource {
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/type")
+    @StarkPermissions(permission = StarkPermissions.Permission.PERMISSION_MANAGEMENT)
     public Response getRoleByType(String type){
         return Response.ok(roleFacade.getRoleByType(type)).build();
     }
