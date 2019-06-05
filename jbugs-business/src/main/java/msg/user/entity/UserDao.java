91a05d8529d7b0938cc53dc1162e6e8b589de3e1
casicampean <casicampean@yahoo.com>
2019-06-05 20:56:31 +0300

    added username generator

--- jbugs-business/src/main/java/msg/user/entity/UserDao.java
+++ jbugs-business/src/main/java/msg/user/entity/UserDao.java
@@ -8,6 +8,7 @@ import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import java.util.HashSet;
 import java.util.List;
 
@@ -82,6 +83,16 @@ public class UserDao {
                 .setParameter(UserEntity.USERNAME,username)
                 .getSingleResult();
         return (count > 0);
+    }
+
+    public List<UserEntity> findUsernames(String username){
+        //List<UserEntity>userEntities = em.createNamedQuery(UserEntity.FIND_USERNAMES, Long.class)
+        //        .setParameter(UserEntity.USERNAME,username+"%")
+         //       .getResultList();
+
+        TypedQuery<UserEntity> query = em.createQuery("SELECT u from UserEntity u where u.username like :" + UserEntity.USERNAME,UserEntity.class);
+        query.setParameter("username", username+"%");
+        return query.getResultList();
     }
 
 
